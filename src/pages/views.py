a0379943b9f45cb8a84c88ccf75b05b39202c09f
Patricio Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-06-21 23:40:09 -0400

    Implementación CRUD

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 from .models import Page
 
 
@@ -7,6 +9,31 @@ class PageListView(ListView):
     model = Page
 
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page': page})
+class PageDetailView(DetailView):
+    model = Page
+
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')  # se ahorra código
+
+    """
+        def get_success_url(self):
+        return reverse('pages:pages')
+    
+    """
+
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id] + '?ok')
+
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
--- src/webplayground/urls.py
+++ src/webplayground/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('pages/', include('pages.urls')),
+    path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
 ]
