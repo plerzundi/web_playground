b2c67678beb55d6894d08890d191aac9ddd498cd
Patricio Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-06-22 02:04:20 -0400

    Creación app registration

--- src/webplayground/urls.py
+++ src/webplayground/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    # path de auth
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
