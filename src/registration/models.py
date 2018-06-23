a22cec00a409fdfeda0de810a233522b2a8bfbac
Patricio Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-06-23 01:22:57 -0400

    Se agrega función para borrar el antiguo avatar.

--- src/registration/models.py
+++ src/registration/models.py
@@ -4,9 +4,16 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 
+# Funcion que consiste en borrar el avatar antiguo y coloca el nuevo
+def custom_upload_to(instance, filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profiles/'+ filename
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
+    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
 
