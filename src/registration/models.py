d6744768239e3ecba938c03e03eb6dd500c373a3
Patricio Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-06-23 01:15:02 -0400

    Creación de pruebas unitarias

--- src/registration/models.py
+++ src/registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -7,3 +9,11 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
+
+
+# disparador que obliga el usuario a configurar su perfil
+@receiver(post_save, sender=User)
+def ensure_profile_exist(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        Profile.objects.get_or_create(user=instance)
+        print("Se acaba de crear un usuario y su perfil enlazado")
--- src/registration/tests.py
+++ src/registration/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+"""
+Algoritmo de prueba para comprobar si el usuario despues
+de crearlo se enlaza con un perfil automáticamente
+"""
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user('test', 'test@test.com', 'test1234')
+
+    def test_profile_exists(self):
+        exists = Profile.objects.filter(user__username='test').exists()
+        self.assertEqual(exists, True)
